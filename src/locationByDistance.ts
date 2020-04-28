bd29ad0232624d980970fc40d3dad5c8a982f8fc
TheCodemonkey <7ilya@gmx.de>
2020-04-28 14:33:12 +0200

    server side sort filtering and paging implemented

--- src/locationByDistance.ts
+++ src/locationByDistance.ts
@@ -8,89 +8,61 @@ const database = admin.firestore()
 const geoFire = new GeoFirestore(database)
 
 
+class LocationDistanceRequest {
+  coordinates: number[] = [];
+  radius: number = 20;
+  desc: boolean = false;
+  pageSize: number = 10;
+  pageIndex: number = 0;
+  categories: string[] = [];
+}
 
-// Cloud Function to get a list of nearby traders where are public
-export const locationByDistance = functions.https.onCall(async (data: any, context: any) => {
+class LocationDistanceResponse {
+  paging:any = {};
+  locations:any[] = [];
+
+  constructor(locations: any[], totalItems: number, pageSize: number, pageIndex: number) {
+
+    this.paging.totalItems = totalItems;
+    this.paging.totalPages = totalItems/pageSize;
+    this.paging.pageIndex = pageIndex;
+    this.paging.pageSize = pageSize;    
+
+    this.locations = locations;
+  }
+}
+
+
+// Cloud Function to get a list of nearby traders where are public, 
+// includes server side filtering and paging logic
+export const locationByDistance = functions.https.onCall(async (request: LocationDistanceRequest, context: any) => {
   
-  const locations = await geoFire.collection('locations').near({
-    center: new firestore.GeoPoint(data.coords[0], data.coords[1]),
-    radius: data.radius
-  }).get();
+  const locations = await geoFire.collection('locations')
+                    .where('categories', 'array-contains-any', request.categories )
+                    .near({
+                      center: new firestore.GeoPoint(request.coordinates[0], request.coordinates[1]),
+                      radius: request.radius
+                    }).get();
 
-  const result = locations.docs.map(d =>  { return { id: d.id, distance: d.distance }})
-                               .sort((a, b) => a.distance - b.distance);
+  let result = locations.docs.map((d) =>  { 
+                              return { 
+                                id: d.id, 
+                                distance: d.distance, 
+                                categories: d.data().categories 
+                            }})
+                            .sort((a, b) => {
+                              return (request.desc === true)? 
+                                b.distance - a.distance : a.distance - b.distance
+                            });
 
-  return result;
+  const totalItems = result.length;
+
+  if (result && result.length > 0) {
+    const start = request.pageIndex * request.pageSize;
+    const end = start + request.pageSize ;
+
+    result = result.slice(start, end);
+  }
+
+  return  new LocationDistanceResponse(result, totalItems, request.pageSize, request.pageIndex);  
 });
-
-
-// /////////////////////////////////////
-// async function getTraders(radius: number, coords: Array<number>, /** resultsPerPage: number, pageNumber: number */){
-  
-//   const query: GeoQuery = geoFire.collection('locations').near({
-//     center: new firestore.GeoPoint(coords[0], coords[1]),
-//     radius,
-//   });
-//   const publicTraderIds = await getPublicTraders(query)
-//   console.log("Starting getTraders")
-//   const allTraders = await query.get()
-//   const tradersNearby: { id: string; distance: number; }[] = []
-
-//   publicTraderIds.forEach(element => {
-//     const traderId = String(allTraders.docs.filter(trader => trader.id === element).map((traders) => traders.id))
-//     const traderDistance = Number(allTraders.docs.filter(trader => trader.id === element).map((traders) => traders.distance))
-//     tradersNearby.push({id: traderId, distance: traderDistance })
-//   });
-
-//   return tradersNearby
- 
-// }
-
-
-// /////////////////////////////////////
-// async function getPublicTraders(allTraders: GeoQuery){
-  
-//   const traderIds = await getTraderIds(allTraders)
-//   console.log("Starting getPublicTraders")
-
-//   var chunkedTraders = []
-//   var chunSize = 10
-//   for (var i=0; i<traderIds.length; i+=chunSize) {
-//     chunkedTraders.push(traderIds.slice(i,i+chunSize))
-//   }
-
-//   const publicTraders = await Promise.all(
-//     chunkedTraders.map((chunk) =>
-//       database.collection('Traders')
-//       .where(admin.firestore.FieldPath.documentId(), 'in', chunk)
-//       .where('status', '==', 'PUBLIC')
-//       .get()
-//     )
-//   )
-
-//   const publicTraderIds: string[] = []
-//   publicTraders.forEach((dataOuter) => 
-//     dataOuter.forEach((dataInner) =>
-//       publicTraderIds.push(dataInner.id)
-//     )
-//   )
-//   return publicTraderIds
-// }
-
-
-
-// /////////////////////////////////////
-// async function getTraderIds(allTraders: GeoQuery){
-  
-//   const shopIds = await new Promise<Array<String>>((resolve) => 
-//     allTraders.onSnapshot((snapshot) => {
-//       const Ids = snapshot.docs.map((traders) => traders.id)
-//       resolve(Ids)
-//     })
-//   )
-//   console.log("Starting getTraderIds")
-  
-//   return shopIds
-// }
-
-
