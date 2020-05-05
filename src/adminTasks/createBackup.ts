45f2bf0c0f3f2c8b38a785a187a61c5cb9f40c51
TheCodemonkey <7ilya@gmx.de>
2020-05-05 15:32:17 +0200

    backup init process fixed. dibugger setup

--- src/adminTasks/createBackup.ts
+++ src/adminTasks/createBackup.ts
@@ -1,5 +1,6 @@
 import {Stage, loadApp, getBackupBucket, callAsync } from './adminStage';
 import * as backupService from '../common/services/db.backup';
+import * as admin from 'firebase-admin';
 
 /*
   call this admin task to create a backup manually
@@ -11,10 +12,17 @@ async function creeateBackup() {
     const projectID = (app.options.credential as any)?.projectId;
     const bucketName = getBackupBucket(stage);
 
+    const credentials = { 
+      client_email: (app.options.credential as any)?.clientEmail, 
+      private_key:  (app.options.credential as any)?.privateKey
+    };
+
+    const firestoreClient = new admin.firestore.v1.FirestoreAdminClient({credentials: credentials, projectId: projectID});
+
     
 
 
-    await backupService.backup(bucketName, projectID, app); 
+    await backupService.backup(bucketName, projectID, app, firestoreClient); 
 }
 
 callAsync(creeateBackup);
